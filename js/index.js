408166b45c5b78d8d065b41b3b2dba26e24fbffe
BOUHET Jerome <jerome.bouhet@maif.fr>
2024-02-27 11:30:56 +0100

    génération du tableau movies et de la function createFilmIn DOM à compléter

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
-const options = {
+async function getListFilms() {
+  const options = {
     method: 'GET',
     headers: {
       accept: 'application/json',
@@ -6,9 +7,66 @@ const options = {
     }
   };
   
-  fetch('https://api.themoviedb.org/3/discover/movie?include_video=false&language=en-US&page=1&sort_by=popularity.desc', options)
+  fetch('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=fr-FR&page=1&sort_by=primary_release_date.asc', options)
     .then(response => response.json())
-    .then(response => console.log(response))
+    .then(response => response.results)
+    .then(movies => {
+      console.log(movies);
+      return movies
+    })
+    .then(movies => movies.forEach(createFilmInDOM))
     .catch(err => console.error(err));
+}
 
-    
+function createFilmInDOM(movie) {
+  console.log(movie)
+  const dest = document.querySelector('section.films')
+  // let movieTitle = document.createElement('h2')
+  // movieTitle.innerText = movies[i].title
+  // articleTitle.appendChild(movieTitle)
+}
+
+getListFilms()
+
+// main()
+
+// window.addEventListener('load', main)
+
+
+
+
+
+
+//=========================================== Exercice de lundi matin à partir du json =======//
+// for (i=0; i<movies.length; i++) {
+
+//   /* afficheage du Titre dans "article" de la "section" */
+
+//   let movieTitle = document.createElement('h2')
+//   movieTitle.innerText = movies[i].title
+//   articleTitle.appendChild(movieTitle)
+
+//   /* afficheage du poster dans "article" de la "section" */
+
+//   let moviePoster = document.createElement('img')
+//   /* version 1*/
+//   // const image_url = new URL(`https://image.tmdb.org/t/p/w500${movies[i].poster_path}`)
+//   // moviePoster.src = image_url
+
+//   /* version 2*/
+//   const image = movies[i].poster_path
+//   moviePoster.src = "https://image.tmdb.org/t/p/w500" + image
+
+
+//   articleTitle.appendChild(moviePoster)
+
+//   /* afficheage du résumé dans "article" de la "section" */
+
+//   let movieOverview = document.createElement('p')
+//   movieOverview.innerText = movies[i].overview
+//   articleTitle.appendChild(movieOverview)
+
+// }
+
+
+
