05879a657ffa0c2389bc911b274bb0fea397c9c2
BOUHET Jerome <jerome.bouhet@maif.fr>
2024-02-27 16:56:23 +0100

    Au clic, changement de page pour afficher le détail du film sélectionné

--- js/index.js
+++ js/index.js
@@ -11,24 +11,30 @@ async function getListFilms() {
     .then(response => response.json())
     .then(response => response.results)
     .then(movies => {
-      console.log(movies);
+      // console.log(movies);
       return movies
     })
     .then(movies => movies.forEach(createFilmInDOM))
     .catch(err => console.error(err));
 }
 
+
 function createFilmInDOM(movie) {
-  console.log(movie)
+  // console.log(movie)
   const dest = document.querySelector('.grid-films')
-  const gridFilm = document.createElement('div')
+  const btn = document.createElement('button')
+  btn.id = movie.id
+  dest.appendChild(btn)
+  // const gridFilm = document.createElement('div')
   let movieTitle = document.createElement('h2')
   movieTitle.innerText = movie.title
-  gridFilm.appendChild(movieTitle)
+  btn.appendChild(movieTitle)
   let moviePoster = document.createElement('img')
   const image = movie.poster_path
+  console.error(moviePoster);
+
   moviePoster.src = "https://image.tmdb.org/t/p/w500" + image
-  dest.appendChild(moviePoster)
+  btn.appendChild(moviePoster)
   // let movieDate = document.createElement('p')
   // movieDate.innerText = movie.release_date
   // dest.appendChild(movieDate)
@@ -38,62 +44,81 @@ function createFilmInDOM(movie) {
   // let movieDate = document.createElement('p')
   // movieDate.innerText = movie.release_date
   // dest.appendChild(movieDate)
+  // document.location.href = `/details.html?id=${idFilm}`
+  function test() {
+    let inputFilm = document.querySelectorAll("button")
+    console.log(inputFilm);
+    inputFilm.forEach((button) => {
+      button.addEventListener('click', (event) => {
+        // const idFilm = btn.id
+        console.log(event.currentTarget.id);
+        document.location.href = `/details.html?id=${event.currentTarget.id}`
+      })
+    })
+  }
+  test();
 }
 
 getListFilms()
 
 
-function characterLoad() {
-  let previewContainer = document.querySelector('.character-preview');
-  let previewBox = previewContainer.querySelectorAll('.preview');
-  let modal = document.getElementById("myModal");
 
-  let dataTargets = [
-      {   
-          target : 'c-1',
-          image : 'images/mao_mao.png',
-          name: 'Mao Mao', 
-          description: `Fille d'un apothicaire vivant à la périphérie de la 
-           et sa goûteuse officielle. `
-      },
 
-      {   
-          target: 'c-2',
-          image : 'images/jinshi2.png',
-          name: 'Jinshi',
-          description: `Eunuque à la beauté surnaturelle, il officie à la cour intérieure où résident les concubines et leur personnel respectif en tant qu'intendant. Alors que son visage attire les hommes et les femmes du palais, il se trouve plutôt attiré par Mao Mao et adore la taquiner car c'est l'unique personne qui reste insensible à son charme.` 
-      },
-    ];
 
-    document.querySelectorAll('.characters-container .character').forEach(character => {
-      character.onclick = () => {
-          let name = character.getAttribute('data-name');
-          console.log(name);
-          dataTargets.forEach(dataTarget => {
-              if (name === dataTarget.target) {
-                  modal.style.display = "block";
-                  previewContainer.innerHTML = `
-                  <div>
-                      <img src="${dataTarget.image}" alt="">
-                      <h3>${dataTarget.name}</h3>
-                      <p>${dataTarget.description}</p>
-                  </div>
-                  `;
-              }
-          });
-      };
-  });
+
+
+
+// function characterLoad() {
+//   let previewContainer = document.querySelector('.character-preview');
+//   let previewBox = previewContainer.querySelectorAll('.preview');
+//   let modal = document.getElementById("myModal");
+
+//   let dataTargets = [
+//       {   
+//           target : 'c-1',
+//           image : 'images/mao_mao.png',
+//           name: 'Mao Mao', 
+//           description: `Fille d'un apothicaire vivant à la périphérie de la 
+//            et sa goûteuse officielle. `
+//       },
+
+//       {   
+//           target: 'c-2',
+//           image : 'images/jinshi2.png',
+//           name: 'Jinshi',
+//           description: `Eunuque à la beauté surnaturelle, il officie à la cour intérieure où résident les concubines et leur personnel respectif en tant qu'intendant. Alors que son visage attire les hommes et les femmes du palais, il se trouve plutôt attiré par Mao Mao et adore la taquiner car c'est l'unique personne qui reste insensible à son charme.` 
+//       },
+//     ];
+
+//     document.querySelectorAll('.characters-container .character').forEach(character => {
+//       character.onclick = () => {
+//           let name = character.getAttribute('data-name');
+//           console.log(name);
+//           dataTargets.forEach(dataTarget => {
+//               if (name === dataTarget.target) {
+//                   modal.style.display = "block";
+//                   previewContainer.innerHTML = `
+//                   <div>
+//                       <img src="${dataTarget.image}" alt="">
+//                       <h3>${dataTarget.name}</h3>
+//                       <p>${dataTarget.description}</p>
+//                   </div>
+//                   `;
+//               }
+//           });
+//       };
+//   });
   
-  window.onclick = function(event) {
-      if (event.target == modal) {
-          modal.style.display = "none";
-      }
-  };
+//   window.onclick = function(event) {
+//       if (event.target == modal) {
+//           modal.style.display = "none";
+//       }
+//   };
   
-  previewBox.forEach(close =>{
-      close.querySelector('.fa-times').onclick = () =>{
-      close.classList.remove('active');
-      previewContainer.style.display = 'none';
-      };
-  });
-}
+//   previewBox.forEach(close =>{
+//       close.querySelector('.fa-times').onclick = () =>{
+//       close.classList.remove('active');
+//       previewContainer.style.display = 'none';
+//       };
+//   });
+// }
