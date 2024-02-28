7c5663e960c0b36e8bf386bb072e28e3aa7be9be
BOURDEAU Jeremy <jeremy.bourdeau@maif.fr>
2024-02-28 16:33:24 +0100

    acteur

--- js/index.js
+++ js/index.js
@@ -7,50 +7,33 @@ async function getListFilms() {
     }
   };
   
-  fetch('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=fr-FR&page=1&primary_release_date.gte=2024-02-27&sort_by=popularity.desc', options)
+fetch('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=fr-FR&page=1&primary_release_date.gte=2024-02-27&sort_by=popularity.desc', options)
     .then(response => response.json())
     .then(response => response.results)
-    // .then(movies => {
-    //   // console.log(movies);
-    //   return movies
-    // })
     .then(movies => movies.forEach(createFilmInDOM))
     .catch(err => console.error(err));
 }
 
 
 function createFilmInDOM(movie) {
-  // console.log(movie)
-  const dest = document.querySelector('.grid-films')
-  const btn = document.createElement('button')
-  btn.id = movie.id
-  dest.appendChild(btn)
-  // const gridFilm = document.createElement('div')
-  let movieTitle = document.createElement('h2')
-  movieTitle.innerText = movie.title
-  btn.appendChild(movieTitle)
-  let moviePoster = document.createElement('img')
-  const image = movie.poster_path
-  console.error(moviePoster);
-
-  moviePoster.src = "https://image.tmdb.org/t/p/w500" + image
-  btn.appendChild(moviePoster)
-  // let movieDate = document.createElement('p')
-  // movieDate.innerText = movie.release_date
-  // dest.appendChild(movieDate)
-  // let movieGenre = document.createElement('p')
-  // movieGenre.innerText = movie.genre_ids
-  // dest.appendChild(movieGenre)
-  // let movieDate = document.createElement('p')
-  // movieDate.innerText = movie.release_date
-  // dest.appendChild(movieDate)
-  // document.location.href = `/details.html?id=${idFilm}`
-  function test() {
+    const dest = document.querySelector('.grid-films')
+    const btn = document.createElement('button')
+    btn.id = movie.id
+    dest.appendChild(btn)
+    let movieTitle = document.createElement('h2')
+    movieTitle.innerText = movie.title
+    btn.appendChild(movieTitle)
+    let moviePoster = document.createElement('img')
+    const image = movie.poster_path
+    moviePoster.src = "https://image.tmdb.org/t/p/w500" + image
+    btn.appendChild(moviePoster)
+  
+  
+function test() {
     let inputFilm = document.querySelectorAll("button")
     console.log(inputFilm);
     inputFilm.forEach((button) => {
       button.addEventListener('click', (event) => {
-        // const idFilm = btn.id
         console.log(event.currentTarget.id);
         document.location.href = `/details.html?id=${event.currentTarget.id}`
       })
